7dff2f41c3edf42925912043aa923578115d72e3
yanghaemi <heau0105@naver.com>
2023-02-22 01:18:25 +0900

    css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Bottombar from "./Component/Bottombar";
+import TimeLine from "./Component/TimeLine";
 
 function App() {
   const userInfo = {
@@ -8,51 +10,6 @@ function App() {
     photo:
       "https://imgscience.ytn.co.kr/sciencetv/jpg/vod1459/2021//202112071802272615_h.jpg",
     introduce: "안녕하세요",
-  };
-
-  const [text, setText] = useState("");
-  const [post, setPost] = useState([]);
-
-  const testApi = async () => {
-    const requestOptions = {
-      title: text,
-      content: text,
-    };
-
-    console.log(requestOptions);
-
-    try {
-      const response = await axios.post(
-        "http://localhost:8000/addblog/",
-        requestOptions
-      );
-      console.log(response);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const reload = async () => {
-    try {
-      const response = await axios.get("http://localhost:8000/showblog");
-      console.log(response);
-      setPost(response.data.content);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  useEffect(() => {
-    reload();
-  }, post);
-
-  const handlePost = () => {
-    if (text !== "") {
-      // setPost((prevList) => [...prevList, text]);
-      setText("");
-      testApi(text);
-      reload();
-    }
   };
 
   return (
@@ -67,86 +24,9 @@ function App() {
         <p>{userInfo.introduce}</p>
       </div>
 
-      <main>
-        <input
-          type="text"
-          placeholder="오늘 기분을 기록해보세요"
-          onChange={(e) => {
-            console.log(e.target.value);
-            setText(e.target.value);
-          }}
-          value={text}
-        ></input>
-        <button onClick={handlePost}>제출</button>
-        <p>{post.length}개의 기록</p>
-        <Article post={post} setPost={setPost}></Article>
-      </main>
+      <TimeLine></TimeLine>
+      <Bottombar></Bottombar>
     </>
   );
 }
-
-const Article = (props) => {
-  let editInputText = "";
-
-  useEffect(() => {
-    console.log(props.post);
-  }, [props.post]);
-
-  const articleDel = async (r) => {
-    const requestOptions = {
-      title: r,
-      content: r,
-    };
-
-    console.log(r);
-
-    try {
-      const response = await axios.post(
-        "http://localhost:8000/delectblog",
-        requestOptions
-      );
-      console.log(response);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  let articles = props.post.map((article, i) => {
-    let removed = () => {
-      articleDel(article);
-      props.post.splice(i, 1);
-      let tempPost = [...props.post];
-      props.setPost(tempPost);
-    };
-
-    let modified = () => {
-      props.setPost((pre) => {
-        let newPost = [...pre];
-        newPost[i] = editInputText;
-        return newPost;
-      });
-    };
-
-    return (
-      <>
-        <div className="article">
-          <p>{article}</p>
-
-          <button onClick={removed}>삭제</button>
-          <form>
-            <input
-              type="text"
-              onChange={(e) => {
-                editInputText = e.target.value;
-              }}
-            ></input>
-            <button onClick={modified}>수정</button>
-          </form>
-        </div>
-      </>
-    );
-  });
-  return <div>{articles}</div>;
-};
-
 export default App;
---
+++ src/Component/Bottombar.js
@@ -0,0 +1,13 @@
+import "../App.css";
+
+const Bottombar = () => {
+  return (
+    <div className="bar">
+      <div className="bottomBarBtn">홈</div>
+      <div className="bottomBarBtn">검색</div>
+      <div className="bottomBarBtn">마이페이지</div>
+    </div>
+  );
+};
+
+export default Bottombar;
